fe5392ded6da28a63155e51fff2de2403d8bfd3c
aashfaque88 <142180273+aashfaque88@users.noreply.github.com>
2023-10-16 15:32:24 +0530

    Add files via upload

---
+++ Python Assignment 1.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Name="Ashfaque"
+print (Name)
+
+
+# In[2]:
+
+
+Name="Rahul"
+print (Name)
+
+
+# In[76]:
+
+
+Name="Ashfaque"
+print ("Hello " +Name,"would you like to learn some Python Today?")
+
+
+# In[79]:
+
+
+print ("Abraham Lincoln said Discipline is choosing between what you want now and what you want most")
+
+
+# In[88]:
+
+
+famous_person= "Abraham Lincoln"
+message= "Discipline is choosing between what you want now and what you want most"
+print (famous_person, "said " + message)
+
+
+# In[89]:
+
+
+print (5+3)
+
+
+# In[91]:
+
+
+print (16-8)
+
+
+# In[92]:
+
+
+print (4*2)
+
+
+# In[95]:
+
+
+print (64/8)
+
+
+# In[113]:
+
+
+fav_numb="Eight"
+print ("My Favourite number is "+fav_numb)
+
+
+# # Ashfaque 10/16/2023 
+
+# In[114]:
+
+
+print ("hello_world")
+
+
+# # Ashfaque 10/16/2023
+
+# In[115]:
+
+
+print ("My name is Ashfaque Shaikh")
+
+
+# In[122]:
+
+
+a= "Tejas"
+b= "Ravi"
+c= "Raj"
+d= "Divya"
+e= "Firdaus"
+f= "Pranali"
+g= "Priya"
+h= "Yash"
+
+print (a)
+
+
+# In[123]:
+
+
+print (b)
+
+
+# In[124]:
+
+
+print (c)
+
+
+# In[125]:
+
+
+print ("Hi "+a, "you are my best friend")
+
+
+# In[126]:
+
+
+print ("Hi "+b, "you are my best friend")
+
+
+# In[127]:
+
+
+print ("Hi "+c, "you are my best friend")
+
+
+# In[128]:
+
+
+print ("Hi "+d, "you are my best friend")
+
+
+# In[129]:
+
+
+print ("Hi "+e, "you are my best friend")
+
+
+# In[ ]:
+
+
+
+
